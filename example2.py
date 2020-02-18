d98be964fc2d7271e0f8b7244d9fdf4d99632094
v8sirisha8v <sirishakvelavan@gmail.com>
2020-02-18 16:48:53 +0000

    Made the program collude every 3rd round (example 2).

--- example2.py
+++ example2.py
@@ -11,18 +11,19 @@ strategy_name = 'Collude every third round'
 strategy_description = 'Collude every third round. Betray all other rounds.'
     
 def move(my_history, their_history, my_score, their_score):
-    '''Make my move based on the history with this player.
-    
-    history: a string with one letter (c or b) per round that has been played with this opponent.
-    their_history: a string of the same length as history, possibly empty. 
-    The first round between these two players is my_history[0] and their_history[0]
-    The most recent round is my_history[-1] and their_history[-1]
-    
-    Returns 'c' or 'b' for collude or betray.
-    '''
-    # This player colludes every third round (first round is round #0).
-    if len(my_history)%3 == 0:
-        return 'c'
-    else:
-        return 'b'
+  '''Make my move based on the history with this player.
+  
+  history: a string with one letter (c or b) per round that has been played with this opponent.
+  their_history: a string of the same length as history, possibly empty. 
+  The first round between these two players is my_history[0] and their_history[0]
+  The most recent round is my_history[-1] and their_history[-1]
+  
+  Returns 'c' or 'b' for collude or betray.
+  '''
+  # This player colludes every third round (first round is round #0).
+  if len(my_history)%3:   
+    return 'c'
+  else:
+    return 'b'
+  rn 'b'
     
